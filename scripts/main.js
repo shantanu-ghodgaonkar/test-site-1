b76f76eeb84302111e163ee0cf012908bbbfed14
Shantanu Ghodgaonkar <shantanu.ghodgaonkar@gmail.com>
2021-04-26 15:10:53 +0530

    JS start

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+alert("Hello!\nPress \"OK\" to continue!");
+
+function multiply(n1, n2)
+{
+	return n1 * n2;
+}
+
+document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
